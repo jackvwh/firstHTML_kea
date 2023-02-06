7b470bec709ec95d2c05e143c3b1c9540143e06d
Jack <Jackvwh@hotmail.com>
2023-02-06 15:17:16 +0100

    last commit for the day

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
-    document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
 // alert("Hello NERD");
 console.log("Hello NERDS");
+
+document.getElementById("first_p");
